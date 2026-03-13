a96f5c1fd6657904fe5f33bb08e12326e253d0e4
jwordz <jwordz@github.com>
2026-03-13 12:41:49 +0000

    Initial Commit

---
+++ QuickLaunch/QuickLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLaunchApp.swift
+//  QuickLaunch
+//
+//  Created by Parys Jordon on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuickLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
